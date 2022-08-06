a78655ffdbb19951d8790437c4ca233bb2e0b22a
Neelay Fruitwala <neelay@lbl.gov>
2022-08-06 01:06:22 -0700

    syndrome jump test passing

--- cocotb/dspunit_lut/drivers.py
+++ cocotb/dspunit_lut/drivers.py
@@ -11,17 +11,13 @@ class DSPUnitLUTDriver(DSPDriver):
     def __init__(self, dut, mon_signals=None):
         super().__init__(dut, mon_signals)
 
-    async def load_meas(self, meas_qb_ind, meas_result, meas_valid):
+    async def load_meas(self, meas_result, meas_valid):
         #dspunit_ind = self.n_dspunit - dspunit_ind - 1 #indexing is backwards on module ports
         if len(meas_result) != len(meas_valid):
             raise Exception('data and ready arrays must be same length')
-        if not np.all((meas_result==0) | (meas_result==1)):
-            raise Exception('meas result must be 0 or 1')
-        if not np.all((meas_valid==0) | (meas_valid==1)):
-            raise Exception('meas valid must be 0 or 1')
         for i in range(len(meas_result)):
             #ipdb.set_trace()
-            self._dut.meas.value = int(meas_result[i]) << meas_qb_ind
-            self._dut.meas_valid.value = int(meas_valid[i]) << meas_qb_ind
+            self._dut.meas.value = int(meas_result[i])
+            self._dut.meas_valid.value = int(meas_valid[i])
             await RisingEdge(self._dut.clk)
 
--- cocotb/dspunit_lut/test_dsp_lut.py
+++ cocotb/dspunit_lut/test_dsp_lut.py
@@ -11,5 +11,40 @@ from drivers import DSPUnitLUTDriver, generate_clock
 from sim_tools import check_pulse_output, debug_plots
 
 @cocotb.test()
-async def const_pulse_out_test(dut):
-    pass
+async def syndrome_unit2_10_out_test(dut):
+    freq = 100.e6
+    phase = np.pi/2
+    pulse_length = 40
+    env_i = 0.2*np.ones(pulse_length)
+    env_q = 0.1*np.ones(pulse_length)
+    ncycles = 200
+
+    pulse_unit_ind = 2
+
+    dspunit = DSPUnitLUTDriver(dut)
+    prog = MultiUnitAssembler(dspunit.n_dspunit)
+    prog.assemblers[pulse_unit_ind].add_jump_fproc(0, 'le', 'loc0', 1)
+    prog.add_pulse(pulse_unit_ind, freq, phase, 10, env_i + 1j*env_q)
+    prog.add_pulse(pulse_unit_ind, 2*freq, phase, 20, 2*env_i + 1j*2*env_q, label='loc0')
+    #prog.add_declare_freq(ph_acc_ind, freq)
+
+    cmd_lists, env_buffers = prog.get_compiled_program()
+    meas_input = np.zeros(ncycles)
+    meas_input[5] = 1
+    meas_ready = np.zeros(ncycles)
+    meas_ready[5] = 1
+    meas_ready[6] = 2
+
+
+    cocotb.start_soon(generate_clock(dut))
+    await dspunit.load_program(cmd_lists)
+    await dspunit.load_env(env_buffers)
+    await dspunit.reset()
+    cocotb.start_soon(dspunit.load_meas(meas_input, meas_ready))
+    await dspunit.monitor_outputs(ncycles)
+
+    #ipdb.set_trace()
+    sim_prog = prog.get_sim_program()[pulse_unit_ind]
+    sim_prog = [sim_prog[-1]]
+    debug_plots(sim_prog, dspunit.dac_i[pulse_unit_ind], dspunit.dac_q[pulse_unit_ind])
+    assert check_pulse_output(sim_prog, dspunit.dac_i[pulse_unit_ind], dspunit.dac_q[pulse_unit_ind])
