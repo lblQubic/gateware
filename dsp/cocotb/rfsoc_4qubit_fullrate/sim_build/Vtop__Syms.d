05ae41a614597f71a5af9b6b1dd02327e08eee8c
Neelay Fruitwala <neelay@lbl.gov>
2023-06-12 14:19:26 -0700

    remove junk files

--- dsp/cocotb/rfsoc_4qubit_fullrate/sim_build/Vtop__Syms.d
+++
@@ -1,9 +0,0 @@
-Vtop__Syms.o: Vtop__Syms.cpp Vtop__Syms.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h Vtop.h Vtop__Dpi.h \
- /usr/local/share/verilator/include/vltstd/svdpi.h Vtop_ifdsp.h \
- Vtop_panzoomtrigif__N4_A9_D100.h Vtop_panzoomtrigif__Na_Ac_D40.h \
- Vtop_elementconn__pi3.h Vtop_ifxma__N4_D100.h Vtop_ammod__N10.h \
- Vtop_ifelement__Ec_EB20_F9_FB80.h Vtop_ifelement__Ec_EB20_F9_FB200.h \
- Vtop_ifelement__E8_EB200_F9_FB200.h Vtop_cmd_mem_iface__C10_M80_MB1.h \
- Vtop_fproc_iface.h Vtop_sync_iface.h Vtop_pulse_iface.h
