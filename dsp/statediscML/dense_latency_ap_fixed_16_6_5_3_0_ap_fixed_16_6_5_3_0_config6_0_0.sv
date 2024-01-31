a5f378099062f58415d0cc97592abd03cbf2a233
freaksie <neelvora27@gmail.com>
2024-01-30 16:05:11 -0800

    Layers compatible with 32 bit weights from bram

--- dsp/statediscML/dense_latency_ap_fixed_16_6_5_3_0_ap_fixed_16_6_5_3_0_config6_0_0.sv
+++ dsp/statediscML/dense_latency_ap_fixed_16_6_5_3_0_ap_fixed_16_6_5_3_0_config6_0_0.sv
@@ -76,11 +76,11 @@ reg signed [DS:0] node2;
 reg signed [DS:0] node3;
 
 
-assign w0 = sdif.sdpara[48];
-assign w1 = sdif.sdpara[49];
-assign w2 = sdif.sdpara[50];
-assign w3 = sdif.sdpara[51];
-assign b = sdif.sdpara[64];
+assign w0 = sdif.sdpara[48][17:0];
+assign w1 = sdif.sdpara[49][17:0];
+assign w2 = sdif.sdpara[50][17:0];
+assign w3 = sdif.sdpara[51][17:0];
+assign b = sdif.sdpara[64][17:0];
 
 
 always @ (posedge sdif.clk) begin
