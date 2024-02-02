7990a188c72f3dd98c6a0135eeb347fdd54953ac
Yilun Xu <yilunxu@lbl.gov>
2024-02-01 18:35:45 -0800

    add buffer, lattency is 22 clk cyc

--- dsp/dsp.sv
+++ dsp/dsp.sv
@@ -95,15 +95,9 @@ generate
 		always @(posedge dspif.clk) begin
 			dspif.addr_sdpara[i] <= paraaddr;
 			sdif[i].sdpara[paraaddr_r3] <= dspif.data_sdpara[i];
-<<<<<<< HEAD
-			// if (parabusy_r3) begin
-			// 	sdif[i].sdpara[paraaddr_r3] <= dspif.data_sdpara[i];
-			// end
-=======
             //if (parabusy_r3) begin
             //    sdif[i].sdpara[paraaddr_r3] <= dspif.data_sdpara[i];
 			//end
->>>>>>> eafcd9455acb35495d5743d5a651398c4a551ec3
 		end
 	end
 endgenerate
